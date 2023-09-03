4e6ddeb79fff0a47f19979376f4e5d6fcd22319f
atman-33 <gpbjk0304@gmail.com>
2023-09-03 19:37:29 +0900

    add giscus and fix layout

--- apps/web/app/[lang]/blog/tags/page.tsx
+++ apps/web/app/[lang]/blog/tags/page.tsx
@@ -21,11 +21,12 @@ export default async function Page() {
         <div className="flex max-w-lg flex-wrap">
           {tagKeys.length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
+            // console.log(t);
             return (
               <div key={t} className="mb-2 mr-5 mt-2">
                 <Tag text={t} />
                 <Link
-                  href={`blog/tags/${slug(t)}`}
+                  href={`/blog/tags/${slug(t)}`}
                   className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
                   aria-label={`View posts tagged ${t}`}
                 >
--- apps/web/components/mobile-nav.tsx
+++ apps/web/components/mobile-nav.tsx
@@ -9,7 +9,6 @@ const MobileNav = () => {
 
   const onToggleNav = () => {
     setNavShow((status) => {
-      // eslint-disable-next-line unicorn/prefer-ternary
       if (status) {
         document.body.style.overflow = 'auto';
       } else {
@@ -37,7 +36,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`dark:bg-gray-950 fixed left-0 top-0 z-10 h-full w-full transform bg-white opacity-95 duration-300 ease-in-out dark:opacity-[0.98] ${
+        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-white opacity-95 duration-300 ease-in-out dark:bg-gray-900 dark:opacity-[0.98] ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
