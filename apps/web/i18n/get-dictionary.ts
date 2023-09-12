c5823d5f342fd2ef54d777c49e85731629dd2757
Atman <41929192+atman-33@users.noreply.github.com>
2023-09-13 08:34:35 +0900

    Update get-dictionary.ts

--- apps/web/i18n/get-dictionary.ts
+++ apps/web/i18n/get-dictionary.ts
@@ -1,5 +1,5 @@
 import 'server-only';
-import type { Locale } from './i18n-config';
+import type { i18n, Locale } from './i18n-config';
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the defalt import for cleaner types
@@ -8,4 +8,5 @@ const dictionaries = {
   ja: () => import('./dictionaries/ja.json').then((module) => module.default)
 };
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+// export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+export const getDictionary = async (locale: Local) => dictionaries[i18n.locales.includes(locale) ? locale : i18n.defaultLocale]();
