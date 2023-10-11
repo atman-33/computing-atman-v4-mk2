769f02ada772ce8fef46e177a79f9f64650503d6
atman-33 <gpbjk0304@gmail.com>
2023-10-11 22:47:20 +0900

    minor commit

--- libs/web/ui-shadcn/src/components/ui/label.tsx
+++ libs/web/ui-shadcn/src/components/ui/label.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-/* eslint-disable @nx/enforce-module-boundaries */
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import { cn } from '@libs/web/ui-shadcn/lib/utils';
 
 const labelVariants = cva(
