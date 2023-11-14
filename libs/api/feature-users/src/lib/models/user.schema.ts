c5bf8b91706553d8b3a8107c4fc251a3020bb6e3
atman-33 <gpbjk0304@gmail.com>
2023-11-14 23:21:04 +0900

    add user models

---
+++ libs/api/feature-users/src/lib/models/user.schema.ts
@@ -0,0 +1,13 @@
+import { AbstractDocument } from '@libs/api/mongoose/shared';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false })
+export class UserDocument extends AbstractDocument {
+  @Prop()
+  email!: string;
+
+  @Prop()
+  password!: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserDocument);
