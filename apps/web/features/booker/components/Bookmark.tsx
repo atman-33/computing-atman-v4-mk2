2f2f183cdd078eb2b90723e119918a8b8d4da7ad
atman-33 <gpbjk0304@gmail.com>
2023-12-19 20:36:14 +0900

    minor commit

--- apps/web/features/booker/components/Bookmark.tsx
+++ apps/web/features/booker/components/Bookmark.tsx
@@ -33,13 +33,13 @@ const Bookmark = () => {
       </div>
 
       <div className="flex items-center justify-between">
-        <div className="text-lg font-bold">{bookmark?.name ?? ''}</div>
+        <div className="ml-4 text-lg font-bold">{bookmark?.name ?? ''}</div>
         <div className="flex items-center gap-4">
           <button>
             <FontAwesomeIcon icon={faEdit} size="lg" />
           </button>
-          <AddLink />
           <DeleteBookmark />
+          <AddLink />
         </div>
       </div>
       <hr className="my-2" />
@@ -55,33 +55,35 @@ const Bookmark = () => {
                   link.description?.toLowerCase().includes(filterVal.toLowerCase()) ||
                   link.siteName?.toLowerCase().includes(filterVal.toLowerCase())
               )
-              .map((link, index) => (
-                <ul key={index}>
-                  <li className="my-2">
-                    <Link href={link.url} target="_blank">
-                      <div className="grid grid-cols-12">
-                        <div className="col-span-2 pt-2">
-                          <Image
-                            src={link.image ?? ''}
-                            alt={link.title ?? ''}
-                            width={80}
-                            height={80}
-                            className="rounded-md"
-                          ></Image>
-                        </div>
-                        <div className="col-span-10 mx-2">
-                          <div className="my-1 font-bold">{link.title}</div>
-                          <div className="my-1 text-xs">{link.description}</div>
-                          <div className="my-1 flex items-start gap-x-2 text-xs">
-                            <FontAwesomeIcon icon={faPager} className="h-[16px]" />
-                            <div>{link.siteName}</div>
+              .map((link) => (
+                <div>
+                  <ul key={link.id} className="p-2 hover:bg-gray-300 dark:hover:bg-gray-700">
+                    <li className="">
+                      <Link href={link.url} target="_blank">
+                        <div className="grid grid-cols-12">
+                          <div className="col-span-2">
+                            <Image
+                              src={link.image ?? ''}
+                              alt={link.title ?? ''}
+                              width={80}
+                              height={80}
+                              className="rounded-md"
+                            ></Image>
+                          </div>
+                          <div className="col-span-10 mx-2">
+                            <div className="my-1 font-bold">{link.title}</div>
+                            <div className="my-1 text-xs">{link.description}</div>
+                            <div className="my-1 flex items-start gap-x-2 text-xs">
+                              <FontAwesomeIcon icon={faPager} className="h-[16px]" />
+                              <div>{link.siteName}</div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  </li>
+                      </Link>
+                    </li>
+                  </ul>
                   <hr />
-                </ul>
+                </div>
               ))}
           </ul>
         </ScrollArea>
