0a3151555f285430f3c99c2ea8d2e37961c93448
atman-33 <gpbjk0304@gmail.com>
2023-12-22 22:15:04 +0900

    change debug folder web

---
+++ apps/web/app/(_debug)/debug/page.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import Link from '@/components/elements/Link';
+
+const debugPages = [
+  '/debug/auth-login-sample',
+  '/debug/auth-test-1',
+  '/debug/auth-test-2',
+  '/debug/hover-button',
+  '/debug/loop-animation',
+  '/debug/ok-cancel-dialog',
+  '/debug/shadcn-ui',
+  '/debug/tailwindcss-sample',
+  '/login',
+  '/signup',
+  '/sites/booker'
+];
+
+const Page = () => {
+  return (
+    <div>
+      {debugPages.map((page) => {
+        return (
+          <div key={page} className="m-2 rounded-md bg-purple-300 p-2">
+            <Link href={page}>{page}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Page;
