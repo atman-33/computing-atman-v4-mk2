da820f4bf8765bb618f92570d3d2bd974f85a6f8
atman-33 <gpbjk0304@gmail.com>
2024-01-01 17:09:53 +0900

    update schema-codegen index.js

--- tools/schema-codegen/index.js
+++ tools/schema-codegen/index.js
@@ -1,6 +1,5 @@
 // ---- Constants ---- //
-// const FOLDER_PATH = './libs/api/prisma/data-access-db/src/lib';
-const FOLDER_PATH = './tools/schema-codegen';
+const FOLDER_PATH = './libs/api/prisma/data-access-db/src/lib';
 const FILE_NAME = 'schema.prisma';
 // ------------------- //
 
@@ -15,36 +14,44 @@ const parsePrismaSchema = (schemaContent) => {
   const models = [];
   let currentModel = null;
 
+  // Split the schema content into lines
   const lines = schemaContent.split('\n').filter((line) => !line.trim().startsWith('//'));
-  lines.forEach((line) => {
-    const modelMatch = line.match(/^model\s+(\w+)\s+\{/);
 
+  // Iterate over each line
+  lines.forEach((line) => {
+    // Check if the line defines a new model
+    const modelMatch = line.match(/^model\s+(\w+)\s+\{/);
     if (modelMatch) {
+      // If a new model is found, push the current model to the models array
       if (currentModel) {
         models.push(currentModel);
       }
+      // Create a new model object
       currentModel = { model: modelMatch[1], columns: [] };
     } else {
+      // Check if the line contains the @id or @relation annotation
       const idMatch = line.match(/@id/);
       const relationMatch = line.match(/@relation/);
 
-      if (!currentModel) {
-        return;
-      }
-      if (relationMatch) {
+      // If there is no current model or the line contains a relation annotation, skip to the next line
+      if (!currentModel || relationMatch) {
         return;
       }
 
+      // Split the line into column name and type
       const columnMatch = line.split(/\s+/).filter(Boolean);
       if (columnMatch && columnMatch.length >= 2) {
         const columnName = columnMatch[0];
         const type = columnMatch[1];
+        // Set the key value based on whether the line contains the @id annotation
         const key = idMatch ? 1 : 0;
+        // Add the column object to the current model's columns array
         currentModel.columns.push({ name: columnName, type, key });
       }
     }
   });
 
+  // Push the last current model to the models array
   if (currentModel) {
     models.push(currentModel);
   }
